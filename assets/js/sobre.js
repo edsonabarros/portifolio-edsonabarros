1014ee626549c4b229abfb179312933ecda32366
edsonabarros <edsonabarros01@gmail.com>
2022-10-02 11:42:45 -0300

    Finalização da função de abrir e fechar arquivo

--- assets/js/sobre.js
+++ assets/js/sobre.js
@@ -1,9 +1,14 @@
-function esconderArquivo(idBotao){
-  var value = document.getElementById(idBotao);
-  value.style.display = "none";
+function esconderArquivo(idBotao, idArquivo) {
+  var valueBotao = document.getElementById(idBotao);
+  var valueArquivo = document.getElementById(idArquivo);
+  valueBotao.style.display = "none";
+  valueArquivo.style.display = "none";
+
 }
 
-function abrirArquivo(idBotao){
-  var value = document.getElementById(idBotao);
-  value.style.display = "flex";
+function abrirArquivo(idBotao, idArquivo) {
+  var valueBotao = document.getElementById(idBotao);
+  var valueArquivo = document.getElementById(idArquivo);
+  valueBotao.style.display = "flex";
+  valueArquivo.style.display = "inline";
 }

8c226933a71b79107985c35ba76d8ef40d85bfc6
Edson Barros <edsonabarros01@gmail.com>
2022-10-25 14:00:47 -0300

    modificação na javascript do sobre

--- assets/js/sobre.js
+++ assets/js/sobre.js
@@ -1,14 +1,33 @@
-function esconderArquivo(idBotao, idArquivo) {
+function esconderArquivo(idBotao, idArquivo, idMostrar) {
   var valueBotao = document.getElementById(idBotao);
   var valueArquivo = document.getElementById(idArquivo);
-  valueBotao.style.display = "none";
-  valueArquivo.style.display = "none";
+  var mostrarArquivo = document.getElementById(idMostrar);
 
+  var display = window.getComputedStyle(valueArquivo, null).display;
+
+  if (display == "block") {
+    alert("Está");
+    valueBotao.style.display = "none";
+    valueArquivo.style.display = "none";
+    mostrarArquivo.style.display = "block";
+
+      //var visibleObject = document.querySelector(".header-li");
+
+
+  } else {
+    alert("Não está!");
+    valueBotao.style.display = "none";
+    valueArquivo.style.display = "none";
+  }
 }
 
-function abrirArquivo(idBotao, idArquivo) {
+function abrirArquivo(idBotao, idArquivo, idEsconder, idEsconder2) {
   var valueBotao = document.getElementById(idBotao);
   var valueArquivo = document.getElementById(idArquivo);
+  var esconderArquivo = document.getElementById(idEsconder);
+  var esconderArquivo2 = document.getElementById(idEsconder2);
   valueBotao.style.display = "flex";
-  valueArquivo.style.display = "inline";
+  valueArquivo.style.display = "block";
+  esconderArquivo.style.display = "none";
+  esconderArquivo2.style.display = "none";
 }

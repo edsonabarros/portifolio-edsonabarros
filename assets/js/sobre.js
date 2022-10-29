5a9c6b4dbab18889b55f4b3242a5d6cc9bad2d36
Edson Barros <edsonabarros01@gmail.com>
2022-10-29 01:54:31 -0300

    Criação dos cards de projeto

--- assets/js/sobre.js
+++ assets/js/sobre.js
@@ -1,31 +1,92 @@
 function esconderArquivo(idBotao, idArquivo, idMostrar) {
-  var valueBotao = document.getElementById(idBotao);
-  var valueArquivo = document.getElementById(idArquivo);
-  var mostrarArquivo = document.getElementById(idMostrar);
+  let valueBotao = document.getElementById(idBotao);
+  let valueArquivo = document.getElementById(idArquivo);
+  let valueMostrar = document.getElementById(idMostrar);
 
-  var display = window.getComputedStyle(valueArquivo, null).display;
+  let vSobre;
+  let vHabilidades;
+  let vEducacao;
 
-  if (display == "block") {
-    alert("Está");
-    valueBotao.style.display = "none";
-    valueArquivo.style.display = "none";
-    mostrarArquivo.style.display = "block";
+  let sobre = document.querySelector("#sobre");
+  let educacao = document.querySelector("#educacao");
+  let habilidades = document.querySelector("#habilidades");
 
-      //var visibleObject = document.querySelector(".header-li");
-
-
+  if (idArquivo === "p-educacao" && habilidades.offsetParent === null) {
+    valueMostrar = document.getElementById("p-sobre");
+    alert("Sobre1");
+  } else if (idArquivo === "p-sobre" && educacao.offsetParent == null) {
+    valueMostrar = document.getElementById("p-educacao");
+    alert("Educa1");
+  } else if (idArquivo === "p-habilidades" && educacao.offsetParent === !null) {
+    alert("Educa 2");
+    valueMostrar = document.getElementById("p-educacao");
   } else {
-    alert("Não está!");
+    valueMostrar = document.getElementById(idMostrar);
+  }
+
+  let displayA = window.getComputedStyle(valueArquivo, null).display;
+
+  if (displayA == "block") {
+    valueArquivo.style.display = "none";
+    valueBotao.style.display = "none";
+    valueMostrar.style.display = "block";
+  } else {
+    valueArquivo.style.display = "none";
+    valueBotao.style.display = "none";
+  }
+
+  if (
+    sobre.offsetParent === null &&
+    educacao.offsetParent === null &&
+    habilidades.offsetParent === null
+  ) {
+    valueArquivo.style.display = "none";
+    valueBotao.style.display = "none";
+    valueMostrar.style.display = "none";
+  }
+  /*     if (
+      sobre.offsetParent === !null &&
+      educacao.offsetParent === null &&
+      habilidades.offsetParent === null
+    ) {
+      vSobre.style.display = "block";
+    } else if (
+      sobre.offsetParent === null &&
+      educacao.offsetParent === !null &&
+      habilidades.offsetParent === null
+    ) {
+      vEducacao.style.display = "block";
+    }else if (
+      sobre.offsetParent === null &&
+      educacao.offsetParent === null &&
+      habilidades.offsetParent === !null
+    ){
+      vHabilidades.style.display = "block";
+    }
+
+  alert(vSobre, vEducacao, vHabilidades);
+
+/  if (
+    sobre.offsetParent === null &&
+    educacao.offsetParent === null &&
+    habilidades.offsetParent === null
+  ) {
     valueBotao.style.display = "none";
     valueArquivo.style.display = "none";
-  }
+    valueMostrar.style.display = "none";
+  } else if (
+    sobre.offsetParent === !null &&
+    educacao.offsetParent === null &&
+    habilidades.offsetParent === null
+  ) {
+    vSobre.style.display = "block";
+  }  */
 }
-
 function abrirArquivo(idBotao, idArquivo, idEsconder, idEsconder2) {
-  var valueBotao = document.getElementById(idBotao);
-  var valueArquivo = document.getElementById(idArquivo);
-  var esconderArquivo = document.getElementById(idEsconder);
-  var esconderArquivo2 = document.getElementById(idEsconder2);
+  let valueBotao = document.getElementById(idBotao);
+  let valueArquivo = document.getElementById(idArquivo);
+  let esconderArquivo = document.getElementById(idEsconder);
+  let esconderArquivo2 = document.getElementById(idEsconder2);
   valueBotao.style.display = "flex";
   valueArquivo.style.display = "block";
   esconderArquivo.style.display = "none";
